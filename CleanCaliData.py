2e0ece599fe5e9eea9dedd4864a0ad2222547968
tomcairns1 <t.cairns97@gmail.com>
2021-01-23 15:15:53 -0500

    Cleaned code

--- CleanCaliData.py
+++ CleanCaliData.py
@@ -15,49 +15,38 @@ import pandas as pd
 
 # Import CSV files into dataframes
 print('begin')
-partiesDf = pd.read_csv('parties.csv', low_memory=False)
+partiesDf = pd.read_csv('parties.csv', usecols=['case_id', 'party_age', 'party_sex', 'party_sobriety'])
 print('Parties table read')
-collisionsDf = pd.read_csv('collisions.csv', low_memory=False)
+collisionsDf = pd.read_csv('collisions.csv', usecols=['case_id', 'collision_date'])
 print('Collisions table read')
 print('Data imported')
 
-# Pull out the columns from collisionsDf that we're interested in
-cleanDf1 = pd.DataFrame()
-cleanDf1['case_id'] = collisionsDf['case_id']
-cleanDf1['date'] = collisionsDf['collision_date']
-print('CleanDf1 obtained')
-
-# Pull out the columns from partiesDf that we're interested in
-cleanDf2 = pd.DataFrame()
-cleanDf2['case_id'] = partiesDf['case_id']
-cleanDf2['party_age'] = partiesDf['party_age']
-cleanDf2['party_sex'] = partiesDf['party_sex']
-cleanDf2['party_sobriety'] = partiesDf['party_sobriety']
-print('CleanDf2 obtained')
-
-# Merge cleanDf1 and cleanDf2
-cleanDf = pd.merge(cleanDf1, cleanDf2, on='case_id', sort=False)
-# sort=F makes it faster and also doesn't reorder the data useful for when we
-# want to look at the date breakdown
+# Merge partiesDf and collisionsDf
+cleanDf = pd.merge(partiesDf, collisionsDf, on='case_id', sort=False) # sort=F makes it faster
 print('Data merged')
+print(cleanDf.head)
 
 #######################
 # Clean and Export Data
 #######################
 
-# Remove any rows where party_age = 998; 998 is an unknown age
+totalLength = len(cleanDf.index)
+print('Total Length is: ', totalLength)
+
 # Remove any rows where party_sobriety is not B; B is under the influence
-for i, row in cleanDf.iterrows():
-    if row['party_age'] == '998' or row['party_sobriety'] != 'B':
-        cleanDf.drop(index=i)
+cleanDf.drop(cleanDf[cleanDf['party_sobriety'] != 'B'].index, inplace=True)
 
-# Write the cleanDf into a new CSV file
-cleanDf.to_csv('clean_data.csv')
+# Remove any rows with unknown values in age or sex columns
+cleanDf = cleanDf[cleanDf['party_age'].notna()]
+cleanDf = cleanDf[cleanDf['party_sex'].notna()]
 
-
-
-
+print(cleanDf.head)
 
 ###################
 # Export Clean Data
 ###################
+
+# Write the cleanDf into a new CSV file
+cleanDf.to_csv('clean_data.csv')
+print('Done')
+
